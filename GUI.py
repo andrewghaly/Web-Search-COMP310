07f01917ddc735a2bf3cb73eff97959b421fae7a
Andrew Ghaly <ghalya@wit.edu>
2015-04-10 14:51:53 -0400

    Updated GUI and WebScrape to work together

--- GUI.py
+++ GUI.py
@@ -1,64 +1,25 @@
-from bs4 import BeautifulSoup
-import urllib2
-import re
 from Tkinter import *
-import Tkinter as tk
+from WebScrape import run
 
-urls = []
-history = []
-
-set_of_links = set()
+master = Tk()
 
 
-class StartPage(tk.Frame):
+label = Label(text="Enter website to start from")
 
-    def __init__(self, master=None):
-            Frame.__init__(self, master)
-            self.root = master
-            self.master.title("WebCrawler")
-            self.pack()
-            self.create_widgets(self)
+entry = Entry()
 
-    def scan_link(self):
-        try:
-            link = entry1.get()
-            site_load = urllib2.urlopen(link)
-            text.insert("\nScraping link: ", link.split("http://")[1])
+title = master.title("Web Crawl")
 
-            current_link = BeautifulSoup(site_load)
-            text.insert(current_link.html.head.title.string)
-            for link in current_link.findAll('a', attrs={'href': re.compile("^http://")}):
-                if link.get('href') not in urls:
-                    urls.append(link.get('href'))
-        except Exception :
-                text.insert("")
+def call():
+    #run(entry) where entry is link entered as paramater to WebScrape
+    run()
 
-        a = self.entry1.get()
-        scan_link(a)
+button = Button(master, text="GO", command=call)
 
-    while urls:
-        for x in urls:
-            text.insert(x)
-        # print len(urls)
+label.pack()
 
-            text.insert(len(urls), "URLs in stack")
-            text.insert(len(history), "URLs in history")
+entry.pack()
+button.pack()
 
-            scan_link(x)
-            urls.pop(0)
-            if x not in history:
-                history.append(x)
-
-    def create_widgets(self, scan_link):
-        label = tk.Label(self, text="Please Enter in a website url to parse")
-        label.grid(row=0, columnspan=2)
-        entry1 = tk.Entry(self)
-        entry1.grid(row=1, column=0, sticky="e")
-        button = tk.Button(self, text="  GO  ", command=scan_link)
-        button.grid(row=1, column=1, sticky="w")
-        text = tk.Text(self)
-        text.grid(row=2, columnspan=2)
-
-app = StartPage()
-app.mainloop()
+mainloop()
 
